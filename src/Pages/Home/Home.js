c4333dcc47d283f829335fd26c6c23ffd81af453
Mujakkir85 <mujakkirlucse@gmail.com>
2022-05-23 23:49:20 +0600

    added reviews design

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Parts from './Parts/Parts';
+import Reviews from './Reviews/Reviews';
 import Summary from './Summary/Summary';
 
 const Home = () => {
@@ -10,8 +11,10 @@ const Home = () => {
             <Banner></Banner>
             <h1 className='font-bold text-4xl text-center text-primary py-10'>Choose Your Parts</h1>
             <Parts></Parts>
-            <h1 className='font-bold text-4xl text-center text-primary py-10'>We Are Best Dealer In Town </h1>
+            <h1 className='font-bold text-4xl text-center text-primary py-16'>We Are Best Dealer In Town </h1>
             <Summary></Summary>
+            <h1 className='font-bold text-4xl text-center text-primary py-16'>User Reviews </h1>
+            <Reviews></Reviews>
 
         </div>
     );
