5dc3a93ea8715adaa3a765411201516d1d612c20
Mujakkir85 <mujakkirlucse@gmail.com>
2022-05-27 00:14:16 +0600

    load a single user to update

--- src/Pages/Dashboard/Myprofile.js
+++ src/Pages/Dashboard/Myprofile.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
+import auth from '../../firebase.init';
+import Loading from '../../Shared/Loading';
+
 
 const Myprofile = () => {
+
+    const [user] = useAuthState(auth);
+
+    const { data: mydata, isLoading, refetch } = useQuery(['singleuser', user.email], () => fetch(`http://localhost:5000/singleuser?email=${user.email}`, {
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+    //.then(data => console.log(data))))
+
+    if (isLoading) {
+        <Loading></Loading>
+    }
+
+    //console.log(data);
     return (
         <div>
-            <h2>My profile</h2>
+            <h2>My profile {mydata?.length}</h2>
         </div>
     );
 };
